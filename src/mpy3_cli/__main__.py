de1a782c06160fbbb850b773d297184199b43cd1
clairox <33704909+clairox@users.noreply.github.com>
2026-06-06 23:46:00 -0400

    feat: implement UI

--- src/mpy3_cli/__main__.py
+++ src/mpy3_cli/__main__.py
@@ -2,5 +2,3 @@ from mpy3_cli.cli import main
 
 if __name__ == "__main__":
     main()
-
-
--- src/mpy3_cli/cli.py
+++ src/mpy3_cli/cli.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import click
 
-from mpy3_cli.playback import PlaybackController as PC
+from mpy3_cli.ui import App
 
 ACCEPTED_FILE_TYPES = [".mp3"]
 
@@ -21,5 +21,5 @@ def main(media_url_input: str):
         print(f"{media_url} is not an mp3 file.")
         sys.exit(1)
 
-    player = PC(media_url)
-    player.play()
+    app = App(media_url)
+    app.run()
